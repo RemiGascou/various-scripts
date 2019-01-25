9a9ca019b765b34673e8503c69ed5b8c162ad698
Remi GASCOU <remi@gascou.net>
2019-01-25 16:59:41 +0100

    Updated ./gitstatusall.sh

--- bash/gitstatusall.sh
+++ bash/gitstatusall.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
 gitstatus () {
+    MAXLEN=30
     cd $1
     if [ -d ".git" ]; then # Checks if this is a git repository
         # Checks git status
         #FETCH=`git fetch`
-        UNABLE_TO_ACCESS=`$FETCH | wc -l`
-
+        # UNABLE_TO_ACCESS=`$FETCH | wc -l`
         UPTODATE=`git status | grep "Your branch is up to date" | wc -l`
+        DIVERGED=`git status | grep "have diverged" | wc -l`
+        DIVERGED_BRANCH=`git status | grep "have diverged" | awk -F 'and' '{print $2}' | awk -F ' ' '{print $1}' | sed "s/'//g"`
         AHEAD=`git status | grep "Your branch is ahead" | wc -l`
         BEHIND=`git status | grep "Your branch is behind" | wc -l`
+
         # Gets the folder name
         BASENAME=`basename $1`
-        # Prints result
-        if [[ $UNABLE_TO_ACCESS -eq '1' ]]; then
-            printf "\x1b[1m%-30s : \x1b[91mUNABLE TO FETCH\x1b[0m\n" ${BASENAME::30}
+
+        if [[ $UPTODATE -eq '1' ]]; then
+            printf "\x1b[1m%-${MAXLEN}s : \x1b[92mUP TO DATE\x1b[0m\n" ${BASENAME::${MAXLEN}}
         else
-            if [[ $UPTODATE -eq '1' ]]; then
-                printf "\x1b[1m%-30s : \x1b[92mUP TO DATE\x1b[0m\n" ${BASENAME::30}
+            if [[ $DIVERGED -eq '1' ]]; then
+                printf "\x1b[1m%-${MAXLEN}s : \x1b[93mDIVERGED with %s\x1b[0m\n" ${BASENAME::${MAXLEN}} $DIVERGED_BRANCH
             else
                 if [[ $AHEAD -eq '1' ]]; then
-                    printf "\x1b[1m%-30s : \x1b[93mAHEAD\x1b[0m\n" ${BASENAME::30}
+                    printf "\x1b[1m%-${MAXLEN}s : \x1b[93mAHEAD\x1b[0m\n" ${BASENAME::${MAXLEN}}
                 else
                     if [[ $BEHIND -eq '1' ]]; then
-                        printf "\x1b[1m%-30s : \x1b[93mBEHIND\x1b[0m\n" ${BASENAME::30}
+                        printf "\x1b[1m%-${MAXLEN}s : \x1b[93mBEHIND\x1b[0m\n" ${BASENAME::${MAXLEN}}
                     else
-                        printf "\x1b[1m%-30s : \x1b[91mUNKNOWN\x1b[0m\n" ${BASENAME::30}
+                        printf "\x1b[1m%-${MAXLEN}s : \x1b[91mUNKNOWN\x1b[0m\n" ${BASENAME::${MAXLEN}}
                     fi
                 fi
             fi
         fi
+        # fi
     fi
     cd ../
 }
